3dceb9458b4708f1264ceb9c1141fac757534e01
tarpaha <ppopchikovsky@gmail.com>
2021-06-16 12:34:06 +0300

    arguments parsing was extracted to module

---
+++ src/args.rs
@@ -0,0 +1,34 @@
+﻿use clap::{Arg, App};
+
+pub fn parse() -> (String, usize, bool) {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(Arg::with_name("folder")
+            .help("Folder to get MD5 from")
+            .required(true))
+        .arg(Arg::with_name("threads")
+            .help("Number of threads, by default equals to cpu count")
+            .short("t")
+            .long("threads")
+            .value_name("count")
+            .takes_value(true))
+        .arg(Arg::with_name("quiet")
+            .help("Quiet mode, only prints resulting MD5")
+            .short("q")
+            .long("quiet")
+            .takes_value(false))
+        .after_help(format!("Usage example: \"md5h .\"\n\
+                             Repository: {}", env!("CARGO_PKG_REPOSITORY")).as_str())
+        .get_matches();
+
+    (
+        matches.value_of("folder").unwrap().parse().unwrap(),
+        match matches.value_of("threads") {
+            None => num_cpus::get(),
+            Some(threads) => threads.parse().unwrap()
+        },
+        matches.is_present("quiet")
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io, env, path::Path, time::Instant};
+use std::{io, path::Path, time::Instant};
 use indicatif::{ProgressBar, ProgressStyle};
 use walkdir::WalkDir;
 use itertools::Itertools;
@@ -6,10 +6,10 @@ use std::fs::File;
 use std::io::Read;
 use std::sync::Arc;
 use tokio::sync::Semaphore;
-use clap::{Arg, App};
 use log::{info};
 
 mod logger;
+mod args;
 
 fn file_md5(filename: impl AsRef<Path>) -> io::Result<String> {
     let mut f = File::open(filename)?;
@@ -36,43 +36,10 @@ fn get_files_recursively(path: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-fn parse_args() -> (String, usize, bool) {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg(Arg::with_name("folder")
-            .help("Folder to get MD5 from")
-            .required(true))
-        .arg(Arg::with_name("threads")
-            .help("Number of threads, by default equals to cpu count")
-            .short("t")
-            .long("threads")
-            .value_name("count")
-            .takes_value(true))
-        .arg(Arg::with_name("quiet")
-            .help("Quiet mode, only prints resulting MD5")
-            .short("q")
-            .long("quiet")
-            .takes_value(false))
-        .after_help(format!("Usage example: \"md5h .\"\n\
-                             Repository: {}", env!("CARGO_PKG_REPOSITORY")).as_str())
-        .get_matches();
-    
-    (
-        matches.value_of("folder").unwrap().parse().unwrap(),
-        match matches.value_of("threads") {
-            None => num_cpus::get(),
-            Some(threads) => threads.parse().unwrap()
-        },
-        matches.is_present("quiet")
-    )
-}
-
 #[tokio::main]
 async fn main() -> Result<(), io::Error> {
-    let (folder, threads, quiet) = parse_args();
-
+    
+    let (folder, threads, quiet) = args::parse();
     logger::init(quiet);
     
     info!("Running in folder {} with {} threads", folder, threads);
